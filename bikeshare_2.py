b5c9c60ade41b941f1f0e83469acd15ee2d3a65e
Sasikant <sasikant.sh@outlook.com>
2019-05-21 18:10:02 -0400

    Refactor: Removed Plotting function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -222,7 +222,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df,city)
-        plot_data(df,city)
+        #plot_data(df,city)
         #raw_data(df)
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
